2a99d1bfc4224996f619b5fa56b107a260333a25
BrunoChrisostomo <91669363+bmchrisostomo19@users.noreply.github.com>
2021-11-28 21:56:22 -0300

    deplou heroku

--- plataforma_cursos/settings.py
+++ plataforma_cursos/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,8 +134,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Configure Django App for Heroku.
-
-django_heroku.settings(locals())
 
